b87e804247d37583bc63f9f9fffcac3a691c8e0f
EMRE ŞAHBUDAK <118505939+EmreSahbudak@users.noreply.github.com>
2023-06-12 02:27:24 +0300

    admin login register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/loginregister/LoginPage';
-import RegisterUserPage from './pages/loginregister/RegisterUserPage';
-import RegisterCompanyPage from './pages/loginregister/RegisterCompanyPage';
+import LoginPage from './pages/loginregisterpages/usercompanyloginregister/LoginPage';
+import RegisterUserPage from './pages/loginregisterpages/usercompanyloginregister/RegisterUserPage';
+import RegisterCompanyPage from './pages/loginregisterpages/usercompanyloginregister/RegisterCompanyPage';
+import AdminLoginPage from './pages/loginregisterpages/adminloginregister/LoginAdminPage'
+import AdminRegisterPage from './pages/loginregisterpages/adminloginregister/AdminRegisterPage';
+import AdminHomePage from './pages/homepages/adminhomepage/AdminHomePage';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterUserPage />} />
       <Route path="/registercompany" element={<RegisterCompanyPage />} />
+      <Route path="/adminlogin" element={<AdminLoginPage />} />
+      <Route path="/adminregister" element={<AdminRegisterPage />} />
+      <Route path="/adminhomepage" element={<AdminHomePage />} />
     </Routes>
   </BrowserRouter>
   );
